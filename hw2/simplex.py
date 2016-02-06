68651dc1f6c3327be54caa8d49323c6ee0c33d0b
Jeff Brandon <jeffbrandon2010@gmail.com>
2016-02-06 09:28:06 -0800

    starting hw2

---
+++ hw2/simplex.py
@@ -0,0 +1,85 @@
+import numpy as np
+
+##########################################################
+# INPUT description:
+# Your algorithms receive 4 inputs:
+# I <- an initial feasible basis
+# c <- a numpy array that you will use as your objective function
+# A,b <- a matrix and vector describing the constraint
+#
+#      A.dot(x) = b
+#
+# OUTPUT description:
+# You algorithms should return two things:
+# 1. the value of the optimal solution v.
+#     - Assuming an optimal solution x, get this with c.dot(x).
+#     - Alternatively, you can use c[I].dot(xI), where xI
+#       is the vector obtained from the optimal basis.
+# 2. an optimal solution x
+#     - This should be in the form of a numpy array.
+#     - Assuming an optimal basis I and associated
+#       inverse of A restricted to index set I, called AI,
+#       you can construct it with:
+#
+#       x = np.zeros(c.shape[0])
+#       x[I] = AI.dot(b)
+#
+# return them with the statement: return (v, x)
+##########################################################
+
+##########################################################
+# Implement any simplex algorithm here.
+# You are free to write a dual and/or revised simplex
+# implementation and call it here. It may be easier to
+# write up the simpler standard simplex algorithm first,
+# since you can reuse a lot of its code for the revised
+# simplex algorithm anyway. It will also be useful as a
+# reference.
+##########################################################
+def simplex(I, c, A, b):
+
+
+##########################################################
+# Implement a simplex algorithm with incremental
+# A inverse computation here.
+# You are free to write a dual simplex with incremental
+# matrix inversion and call it here.
+# In addition to output correctness, this algorithm
+# will also be tested for speed. In our reference
+# implementation, the speedup was a factor of 10-20
+# for most instances.
+##########################################################
+def revised_simplex(I, c, A, b):
+
+
+##########################################################
+# Implement the dual simplex algorithm.
+##########################################################
+def dual_simplex(I, c, A, b):
+
+##########################################################
+# Implement a method that add the new constraint g.T.dot(x) <= h
+# to the existing system of equations
+#
+#    A.dot(x)=b.
+#
+# Then use dual simplex to solve this problem. You can assume
+# that I is an index set which will yield a dual-feasible basis.
+#
+# OUTPUT description:
+# You algorithms should return two things:
+# 1. the value of the optimal solution v (for the new problem).
+#     - Assuming an optimal solution x, get this with c.dot(x).
+#     - Alternatively, you can use c[I].dot(xI), where xI
+#       is the vector obtained from the optimal basis.
+# 2. an optimal solution x (for the new problem).
+#     - This should be in the form of a numpy array.
+#     - Assuming an optimal basis I and associated
+#       inverse of A called AI, you can construct
+#       it with:
+#       x = np.zeros(c.shape[0])
+#       x[I] = AI.dot(b)
+#
+# return them with the statement: return (v, x)
+##########################################################
+def add_constraint(I,c,A,b,g,h):
