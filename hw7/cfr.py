c7e02f03ef4ae854dcba9488855b643c76369a7e
Jeff Brandon <jeffbrandon2010@gmail.com>
2016-04-26 22:56:39 -0400

    progress, not there yet

--- hw7/cfr.py
+++ hw7/cfr.py
@@ -1,8 +1,9 @@
 import sys
+import numpy as np
 from game import Game
 
 
-
+regret = {}
 
 # game will be an instance of Game, which is defined in game.py
 # num_iterations is the number of CFR iterations you should perform.
@@ -25,11 +26,62 @@ def solve_game(game, num_iterations):
     #    strategy_profile[1][1] = [0.666667, 0.333333]
     strategy_profile = {0:{}, 1:{}}
 
+    for node in range(game.num_nodes):
+        tmp = {}
+        if not game.is_leaf(node):
+            n = game.get_num_actions_node(node)
+            for i in game.node_action_names[node]:
+                tmp[i] = 1/float(n)
+            p = game.get_current_player(node)
+            strategy_profile[p][node] = tmp
+
+    print strategy_profile
     #######################
     # Implement CFR in here
     #######################
+    for i in range(num_iterations):
+        print cfr(game, game.get_root(), [1, 1], strategy_profile)
 
     return strategy_profile
+
+def cfr(game, node, reach, sp):
+    #game.print_node_info(node)
+    if game.is_leaf(node):
+        u = game.get_leaf_utility(node)
+        return np.array([u * reach[0], -u * reach[1]])
+    player = game.get_current_player(node)
+    infoset = game.get_node_infoset(node)
+
+    ev = np.zeros(2)
+    actions = game.node_action_names[node]
+    if(reach[player] > 0 ):
+        print "reach"
+        for i, a in enumerate(actions):
+            new_reach = []
+            new_reach.append(reach[0] * sp[player][node][a])
+            new_reach.append(reach[1] * sp[player][node][a])
+            print ev
+            ev += cfr(game, game.get_child_id(node, i), new_reach, sp)
+            print ev
+    else:
+        print "no reach"
+        action_ev = {}
+        opponent = 1-player
+        for i, a in enumerate(actions):
+            p = sp[player][node][a]
+            sp[player][node][a] += reach[player] * p
+            new_reach[player] = reach[player] * p
+            new_reach[opponent] = reach[opponent]
+            action_ev[a] = cfr(game, game.get_child_id(node, a), new_reach, sp)
+            ev[player] += p * action_ev[a][player]
+            ev[opponent] += action_ev[opponent]
+        #for a in actions:
+    return normalize(ev)
+
+def normalize(ev):
+    s = ev[0]+ev[1]
+    print ev
+    return [ev[0]/s,ev[1]/s]
 
 
 
